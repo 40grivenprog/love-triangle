100bc1c2cc3fbeda599c4375acc85eadb0a88613
40grivenprog <43151399+40grivenprog@users.noreply.github.com>
2018-09-20 23:54:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
  * @param preferences - an array of integers. Indices of people, whom they love
  * @returns number of love triangles
  */
-/* module.exports = */ function getLoveTrianglesCount(preferences = []) {
+module.exports = function getLoveTrianglesCount(preferences = []) {
 	var n = preferences.length;
 	var ans = 0;
 	for (var i = 0; i < n; i++) {
